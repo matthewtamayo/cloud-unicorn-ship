abfcb80cbe675984c9bb97f5ae2a48b09de71ab9
John Dexter Reyes <john_dexter.reyes@telus.com>
2022-06-15 08:00:00 +0800

    feat: define tag for operations

--- src/main/java/com/telus/shipping/controller/CityController.java
+++ src/main/java/com/telus/shipping/controller/CityController.java
@@ -1,5 +1,6 @@
 package com.telus.shipping.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import com.telus.shipping.repository.CityRepository;
  */
 @RequestMapping("/city")
 @RestController
+@Tag(name = "city")
 public class CityController {
 
 	@Autowired
