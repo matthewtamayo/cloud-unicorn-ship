0d4fb6fef89a6c45213306cf4d86bc3439d6ae36
John Dexter Reyes <john_dexter.reyes@telus.com>
2022-06-15 08:00:00 +0800

    feat: add city controller

---
+++ src/main/java/com/telus/shipping/controller/CityController.java
@@ -0,0 +1,16 @@
+package com.telus.shipping.controller;
+
+import com.telus.shipping.domain.City;
+
+/**
+ * The {@link City} REST controller.
+ *
+ * @author John Dexter Reyes
+ */
+public class CityController {
+
+	public Iterable<City> get() {
+		return null;
+	}
+
+}
