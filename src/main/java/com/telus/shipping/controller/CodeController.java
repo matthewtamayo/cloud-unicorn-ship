31fa1bf77e299033572e2027b596e69ceca9bc86
John Dexter Reyes <john_dexter.reyes@telus.com>
2022-06-15 08:00:00 +0800

    feat: add code controller

---
+++ src/main/java/com/telus/shipping/controller/CodeController.java
@@ -0,0 +1,16 @@
+package com.telus.shipping.controller;
+
+import com.telus.shipping.domain.Code;
+
+/**
+ * The {@link Code} REST controller.
+ *
+ * @author John Dexter Reyes
+ */
+public class CodeController {
+
+	public Iterable<Code> get() {
+		return null;
+	}
+
+}
