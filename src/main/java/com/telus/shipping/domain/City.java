425fd9cfe4f47bebd032c42ab406d696d9f75e33
John Dexter Reyes <john_dexter.reyes@telus.com>
2022-06-15 08:00:00 +0800

    feat: update city bean

--- src/main/java/com/telus/shipping/domain/City.java
+++ src/main/java/com/telus/shipping/domain/City.java
@@ -1,13 +1,29 @@
 package com.telus.shipping.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 /**
  * The city bean.
  *
  * @author John Dexter Reyes
  */
+@AllArgsConstructor
+@Data
 public class City {
 
-	public City(Long uuid, String code, String city, String name, String region, Double latitude, Double longitude) {
-	}
+	private Long uuid;
+
+	private String code;
+
+	private String city;
+
+	private String name;
+
+	private String region;
+
+	private Double latitude;
+
+	private Double longitude;
 
 }
