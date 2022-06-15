c9021cc5bc9731d54d1c42f0788dba7e19a589df
John Dexter Reyes <john_dexter.reyes@telus.com>
2022-06-15 08:00:00 +0800

    feat: update code controller

--- src/main/java/com/telus/shipping/controller/CodeController.java
+++ src/main/java/com/telus/shipping/controller/CodeController.java
@@ -1,16 +1,28 @@
 package com.telus.shipping.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.telus.shipping.domain.Code;
+import com.telus.shipping.repository.CodeRepository;
 
 /**
  * The {@link Code} REST controller.
  *
  * @author John Dexter Reyes
  */
+@RequestMapping("/code")
+@RestController
 public class CodeController {
 
+	@Autowired
+	private CodeRepository repository;
+
+	@GetMapping
 	public Iterable<Code> get() {
-		return null;
+		return this.repository.findAll();
 	}
 
 }
