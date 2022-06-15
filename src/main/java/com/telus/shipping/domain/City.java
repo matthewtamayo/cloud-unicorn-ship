bfe964ce638db1eabeeeff422672a1c260e15b18
John Dexter Reyes <john_dexter.reyes@telus.com>
2022-06-15 08:00:00 +0800

    feat: add city bean

---
+++ src/main/java/com/telus/shipping/domain/City.java
@@ -0,0 +1,13 @@
+package com.telus.shipping.domain;
+
+/**
+ * The city bean.
+ *
+ * @author John Dexter Reyes
+ */
+public class City {
+
+	public City(Long uuid, String code, String city, String name, String region, Double latitude, Double longitude) {
+	}
+
+}
