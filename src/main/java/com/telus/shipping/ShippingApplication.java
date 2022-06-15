0c736728f956c97706ec58fc7bdcc416c6c0d0b1
John Dexter Reyes <john_dexter.reyes@telus.com>
2022-06-15 08:00:00 +0800

    chore: add doc comments

--- src/main/java/com/telus/shipping/ShippingApplication.java
+++ src/main/java/com/telus/shipping/ShippingApplication.java
@@ -3,9 +3,18 @@ package com.telus.shipping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The shipping application.
+ *
+ * @author John Dexter Reyes
+ */
 @SpringBootApplication
 public class ShippingApplication {
 
+	/**
+	 * The main method for running the application.
+	 * @param args the application arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ShippingApplication.class, args);
 	}
--- src/test/java/com/telus/shipping/ShippingApplicationTests.java
+++ src/test/java/com/telus/shipping/ShippingApplicationTests.java
@@ -3,6 +3,11 @@ package com.telus.shipping;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * The {@link ShippingApplication} tests.
+ *
+ * @author John Dexter Reyes
+ */
 @SpringBootTest
 class ShippingApplicationTests {
 
