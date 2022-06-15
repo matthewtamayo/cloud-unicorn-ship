c6b701564199e9e0f0fa11e687a2112f664a3d2e
John Dexter Reyes <john_dexter.reyes@telus.com>
2022-06-15 08:00:00 +0800

    test: add city repository integration tests

---
+++ src/test/java/com/telus/shipping/repository/CityRepositoryIT.java
@@ -0,0 +1,33 @@
+package com.telus.shipping.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import com.telus.shipping.domain.City;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * The {@link CityRepository} integration tests.
+ *
+ * @author John Dexter Reyes
+ */
+@DataJpaTest
+class CityRepositoryIT {
+
+	@Autowired
+	private CityRepository repository;
+
+	@Autowired
+	private TestEntityManager entityManager;
+
+	@Test
+	void shouldReturnAllCities() {
+		City city = new City(3396054L, "ca", "abbey", "Abbey", "11", 50.7333330, -108.7500000);
+		this.entityManager.persist(city);
+		assertThat(this.repository.findAll()).containsOnly(city);
+	}
+
+}
