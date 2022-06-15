3507c38708dee190c79499c0dc021a18477bec46
John Dexter Reyes <john_dexter.reyes@telus.com>
2022-06-15 08:00:00 +0800

    feat: update city controller

--- src/main/java/com/telus/shipping/controller/CityController.java
+++ src/main/java/com/telus/shipping/controller/CityController.java
@@ -1,16 +1,28 @@
 package com.telus.shipping.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.telus.shipping.domain.City;
+import com.telus.shipping.repository.CityRepository;
 
 /**
  * The {@link City} REST controller.
  *
  * @author John Dexter Reyes
  */
+@RequestMapping("/city")
+@RestController
 public class CityController {
 
+	@Autowired
+	private CityRepository repository;
+
+	@GetMapping
 	public Iterable<City> get() {
-		return null;
+		return this.repository.findAll();
 	}
 
 }
