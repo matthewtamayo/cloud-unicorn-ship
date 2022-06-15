9820ae329a6ea6aeaf56c222a1aff799f2a82ae0
John Dexter Reyes <john_dexter.reyes@telus.com>
2022-06-15 08:00:00 +0800

    feat: add code bean

---
+++ src/main/java/com/telus/shipping/domain/Code.java
@@ -0,0 +1,13 @@
+package com.telus.shipping.domain;
+
+/**
+ * The code bean.
+ *
+ * @author John Dexter Reyes
+ */
+public class Code {
+
+	public Code(Long uuid, String code, String name) {
+	}
+
+}
