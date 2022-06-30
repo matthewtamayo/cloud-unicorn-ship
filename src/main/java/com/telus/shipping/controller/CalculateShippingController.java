9a8f7fe86111cfaa6e4330b3092aa8ca49f28921
Jan O'Neil Vida <jano'neil.vida@telus.com>
2022-06-30 08:00:00 +0800

    feat: update calculate shipping controller

--- src/main/java/com/telus/shipping/controller/CalculateShippingController.java
+++ src/main/java/com/telus/shipping/controller/CalculateShippingController.java
@@ -1,17 +1,31 @@
 package com.telus.shipping.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.telus.shipping.domain.City;
 import com.telus.shipping.domain.Ship;
+import com.telus.shipping.util.Calculator;
 
 /**
  * The calculate shipping task REST controller.
  *
  * @author Jan O'Neil Vida
  */
+@RequestMapping("/calculateShipping")
+@RestController
 public class CalculateShippingController {
 
-	public Ship post(City city) {
-		return null;
+	@Autowired
+	private Calculator calculator;
+
+	@PostMapping
+	public Ship post(@RequestBody City city) {
+		long distance = this.calculator.getDistance(city);
+		return new Ship(distance, (double) (distance * 5) / 100);
 	}
 
 }
