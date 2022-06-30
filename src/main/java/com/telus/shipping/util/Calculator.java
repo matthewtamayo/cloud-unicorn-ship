03e4b5e80c19462b5501e88064543c0d3f528f2c
Jan O'Neil Vida <jano'neil.vida@telus.com>
2022-06-30 08:00:00 +0800

    feat: add distance calculator

---
+++ src/main/java/com/telus/shipping/util/Calculator.java
@@ -0,0 +1,21 @@
+package com.telus.shipping.util;
+
+import com.telus.shipping.domain.City;
+
+/**
+ * Distance calculator.
+ *
+ * @author Jan O'Neil Vida
+ */
+public class Calculator {
+
+	/**
+	 * Calculates the distance of a given city from a fixed home city.
+	 * @param city the given city
+	 * @return the distance from a fixed home city
+	 */
+	public long getDistance(City city) {
+		return 0L;
+	}
+
+}
