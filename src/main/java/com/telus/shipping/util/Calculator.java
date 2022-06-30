3f1a7b314a58ac2a8244c55dc71aaf5b14dc2721
Jan O'Neil Vida <jano'neil.vida@telus.com>
2022-06-30 08:00:00 +0800

    feat: update distance calculator

--- src/main/java/com/telus/shipping/util/Calculator.java
+++ src/main/java/com/telus/shipping/util/Calculator.java
@@ -1,5 +1,7 @@
 package com.telus.shipping.util;
 
+import org.springframework.stereotype.Component;
+
 import com.telus.shipping.domain.City;
 
 /**
@@ -7,7 +9,12 @@ import com.telus.shipping.domain.City;
  *
  * @author Jan O'Neil Vida
  */
+@Component
 public class Calculator {
+
+	private final double HOME_LATITUDE_R = Math.toRadians(51.164896);
+
+	private final double HOME_LONGITUDE_R = Math.toRadians(7.068792);
 
 	/**
 	 * Calculates the distance of a given city from a fixed home city.
@@ -15,7 +22,17 @@ public class Calculator {
 	 * @return the distance from a fixed home city
 	 */
 	public long getDistance(City city) {
-		return 0L;
+		double targetLong = Math.toRadians(city.getLongitude());
+		double targetLat = Math.toRadians(city.getLatitude());
+		// Haversine formula
+		double longDistance = this.HOME_LONGITUDE_R - targetLong;
+		double latDistance = this.HOME_LATITUDE_R - targetLat;
+		double a = Math.pow(Math.sin(latDistance / 2), 2)
+				+ Math.cos(targetLat) * Math.cos(this.HOME_LATITUDE_R) * Math.pow(Math.sin(longDistance / 2), 2);
+		double c = 2 * Math.asin(Math.sqrt(a));
+		// Radius of Earth in kilometers
+		final double earthRadius = 6371;
+		return (long) (c * earthRadius);
 	}
 
 }
