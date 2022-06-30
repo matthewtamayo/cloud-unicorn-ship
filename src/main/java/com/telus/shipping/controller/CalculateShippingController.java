4211278ac18f81313623d5c1480773aeeafab28d
Jan O'Neil Vida <jano'neil.vida@telus.com>
2022-06-30 08:00:00 +0800

    feat: add calculate shipping controller

---
+++ src/main/java/com/telus/shipping/controller/CalculateShippingController.java
@@ -0,0 +1,17 @@
+package com.telus.shipping.controller;
+
+import com.telus.shipping.domain.City;
+import com.telus.shipping.domain.Ship;
+
+/**
+ * The calculate shipping task REST controller.
+ *
+ * @author Jan O'Neil Vida
+ */
+public class CalculateShippingController {
+
+	public Ship post(City city) {
+		return null;
+	}
+
+}
