3aea64f5e69d33ce2dbc9f4cd560746f47af3b23
Jan O'Neil Vida <jano'neil.vida@telus.com>
2022-06-30 08:00:00 +0800

    feat: add shipping information bean

---
+++ src/main/java/com/telus/shipping/domain/Ship.java
@@ -0,0 +1,13 @@
+package com.telus.shipping.domain;
+
+/**
+ * The shipping information bean.
+ *
+ * @author Jan O'Neil Vida
+ */
+public class Ship {
+
+	public Ship(long distance, double cost) {
+	}
+
+}
