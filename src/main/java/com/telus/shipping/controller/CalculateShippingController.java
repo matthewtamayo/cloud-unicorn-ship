25d65f2fc58759697505a31175743d7d2b6c75d1
Jan O'Neil Vida <jano'neil.vida@telus.com>
2022-06-30 08:00:00 +0800

    feat: define tag for operations

--- src/main/java/com/telus/shipping/controller/CalculateShippingController.java
+++ src/main/java/com/telus/shipping/controller/CalculateShippingController.java
@@ -1,5 +1,6 @@
 package com.telus.shipping.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import com.telus.shipping.util.Calculator;
  */
 @RequestMapping("/calculateShipping")
 @RestController
+@Tag(name = "calculateShipping")
 public class CalculateShippingController {
 
 	@Autowired
